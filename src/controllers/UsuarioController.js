b440b2028625f0d11334b6975218490a3e4b595e
AndreaCaroliana <79027755+AndreaCaroliana@users.noreply.github.com>
2022-11-29 07:34:44 -0400

    list user

--- src/controllers/UsuarioController.js
+++
@@ -1,9 +0,0 @@
-const Usuario = require('../models')
-const UsuarioController = {};
-
-UsuarioController.index = (req, res) => {
-    const usuario = Usuario.get();
-    res.render('usuario', (usuario));
-}
-
-module.exports = UsuarioController;
--- src/models/Vivienda.js
+++
@@ -1,9 +0,0 @@
-const pool = require('../database/db.js');
-const Vivienda = {};
-
-Vivienda.get = async () =>{
-    return await pool.query('SELECT v.id as id, v.nombre as n, v.descripcion as des , v.direccion  as dir, v.habitaciones as h, v.banios as bn, v.mt2 as m ,t.descripcion as dd FROM vivienda v INNER JOIN tvivienda t ON v.id_vivenda = t.id');
-};
-
-
-module.exports = Vivienda;
